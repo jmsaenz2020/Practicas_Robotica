4ab530a61625a4f531070a50d832af0a39ed9e4d
Jose Manuel Saenz Latorre <jsmnlsnz@f-l3207-pc43.aulas.etsit.urjc.es>
2023-02-13 13:44:11 +0100

    Practica vacuum-cleaner

--- P1/vacuum_cleaner.py
+++ P1/vacuum_cleaner.py
@@ -4,38 +4,61 @@ import rospy, random, math, time
 import numpy as np
 # Enter sequential code!
 pi = math.pi
-v = 0.15
-w = 2*pi*0.85
-angle = random.uniform(1.6, 2.4)
+v = 1
+w = 2*pi*1
 time_start = time.time()
+start = time_start
 
-def bumped():
+def bumped(w):
     bumper = HAL.getBumperData().bumper
     crash = bool(HAL.getBumperData().state)
     if crash:
-        print("crashed")
-        HAL.setV(-5)
-        HAL.setW(0)
-        rospy.sleep(0.5)
-        HAL.setV(0)
-        HAL.setW(angle*pi)
-        rospy.sleep(0.5)
-    return crash
-
+        if bumper == 0:
+            print("crashed")
+            angle = random.uniform(0, 1.6)
+            HAL.setV(-5)
+            HAL.setW(0)
+            rospy.sleep(0.5)
+            HAL.setV(0)
+            HAL.setW(angle*pi)
+            rospy.sleep(0.5)
+        elif bumper == 2:
+            w *= -1
+            angle = random.uniform(-1.6, 0)
+            HAL.setV(-5)
+            HAL.setW(0)
+            rospy.sleep(0.5)
+            HAL.setV(0)
+            HAL.setW(angle*pi)
+            rospy.sleep(0.5)
+        else:
+            angle = random.uniform(1.6, 2.4)
+            HAL.setV(-5)
+            HAL.setW(0)
+            rospy.sleep(0.5)
+            HAL.setV(0)
+            HAL.setW(angle*pi)
+            rospy.sleep(0.5)
+    return crash, w
 
 while True:
-    HAL.setV(v)
-    HAL.setW(w)
-    bump = bumped()
-    if not bump:
-        if (w >= 0.05):
-            time_current = time.time()
-            if (time_current - time_start) > (2*pi/w):
-                time_start = time.time()
-                v += 0.35
-                w -= 0.35
-            print("V:", v)
-            print("W:", w)
+    end = time.time()
+    if (end - start) > 300:
+        HAL.setV(0)
+        HAL.setW(0)
     else:
-        v = 0.45
-        w = 2*pi*0.55
+        HAL.setV(v)
+        HAL.setW(w)
+        bump, w = bumped(w)
+        if not bump:
+            if (w >= 0.05):
+                time_current = time.time()
+                if (time_current - time_start) > (2*pi/w):
+                    time_start = time.time()
+                    v += 1.4
+                    w -= 0.6
+                print("V:", v)
+                print("W:", w)
+        else:
+            v = 1
+            w = 2*pi*1
