c248cd9257a76be3bbcbbf44c7fc4b7fbe52e52f
Jose Manuel Saenz Latorre <jsmnlsnz@f-l3103-pc07.aulas.etsit.urjc.es>
2023-02-13 17:20:43 +0100

    Practica vacuum-cleaner

--- P1/vacuum_cleaner.py
+++ P1/vacuum_cleaner.py
@@ -5,7 +5,7 @@ import numpy as np
 # Enter sequential code!
 pi = math.pi
 v = 1
-w = 2*pi*1
+w = v*pi
 time_start = time.time()
 start = time_start
 
@@ -14,36 +14,30 @@ def bumped(w):
     crash = bool(HAL.getBumperData().state)
     if crash:
         if bumper == 0:
-            print("crashed")
-            angle = random.uniform(0, 1.6)
-            HAL.setV(-5)
-            HAL.setW(0)
-            rospy.sleep(0.5)
-            HAL.setV(0)
-            HAL.setW(angle*pi)
-            rospy.sleep(0.5)
+            print("izquierda")
+            angle = random.uniform(4.08, 4.71)/pi
+            if w < 0:
+                w *= -1
         elif bumper == 2:
-            w *= -1
-            angle = random.uniform(-1.6, 0)
-            HAL.setV(-5)
-            HAL.setW(0)
-            rospy.sleep(0.5)
-            HAL.setV(0)
-            HAL.setW(angle*pi)
-            rospy.sleep(0.5)
+            print("derecha")
+            angle = random.uniform(1.57, 2.2)/pi
+            if w > 0:
+                w *= -1
         else:
-            angle = random.uniform(1.6, 2.4)
-            HAL.setV(-5)
-            HAL.setW(0)
-            rospy.sleep(0.5)
-            HAL.setV(0)
-            HAL.setW(angle*pi)
-            rospy.sleep(0.5)
+            print("frente")
+            angle = random.uniform(2.83, 3.46)/pi
+        HAL.setV(-2)
+        HAL.setW(0)
+        rospy.sleep(1)
+        HAL.setV(0)
+        HAL.setW(angle)
+        rospy.sleep(1)
     return crash, w
 
 while True:
     end = time.time()
     if (end - start) > 300:
+        print("FINISH")
         HAL.setV(0)
         HAL.setW(0)
     else:
@@ -51,14 +45,11 @@ while True:
         HAL.setW(w)
         bump, w = bumped(w)
         if not bump:
-            if (w >= 0.05):
+            print(v, w)
+            if (abs(w) >= 0.05):
                 time_current = time.time()
-                if (time_current - time_start) > (2*pi/w):
+                if (time_current - time_start) > (2*pi/abs(w)):
                     time_start = time.time()
-                    v += 1.4
-                    w -= 0.6
-                print("V:", v)
-                print("W:", w)
+                    v += 0.5
         else:
             v = 1
-            w = 2*pi*1
