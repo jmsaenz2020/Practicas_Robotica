22b015c874e2d47c39635183b019c511ee00f6e8
Jose Manuel Saenz Latorre <jsmnlsnz@f-l3208-pc19.aulas.etsit.urjc.es>
2023-02-28 10:56:24 +0100

    Practica follow-line

--- P2/follow-line.py
+++ P2/follow-line.py
@@ -1,13 +1,19 @@
 from GUI import GUI
 from HAL import HAL
-import math, cv2
+import math
+import cv2
 import numpy as np
 
-v = 1
-w = 0
-k_p = 0.001
-k_i = 0.1
-k_d = 0.1
+V = 1
+Vmax = 10
+W = 0
+K_P = 1
+K_D = 0.5
+K_I = 0
+e = math.e
+prev_error = 0
+sum_error = 0
+i = 0
 
 def filter_image(image):
     img_hsv=cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -15,7 +21,6 @@ def filter_image(image):
     lower_red = np.array([0,50,50])
     upper_red = np.array([10,255,255])
     mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
-    
     lower_red = np.array([170,50,50])
     upper_red = np.array([180,255,255])
     mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
@@ -26,53 +31,44 @@ def filter_image(image):
     contour, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     max_contour = max(contour, key=cv2.contourArea)
     M = cv2.moments(max_contour) # Obtener centroide de imagen
-    center_x = int(M["m10"] / M["m00"])
-    center_y = int(M["m01"] / M["m00"])
-    center = (center_x, center_y)
+    if M["m00"] != 0:
+        center_x = int(M["m10"] / M["m00"])
+        center_y = int(M["m01"] / M["m00"]) - 100
+        center = (center_x, center_y)
+    else:
+        center = (329, 399)
+
     radius = 5
     color = (255, 0, 0)
     thickness = -1
     image = cv2.circle(image, center, radius, color, thickness)
     GUI.showImage(image)
     return center
-    
-    
-def get_speed(v, w, reference):
-    center = (329, 399)
+
+
+def get_speed(V, W, reference, prev_error, sum_error, i):
+    center = (329, 399) # 329 y 399 son coordenadas del centroide en t = 0
     get_x, get_y = reference
-    diff_x = center[0] - get_x # 329 y 399 son coordenadas del centroide en t = 0
+    diff_x = center[0] - get_x
     diff_y = center[1] - get_y
-    print(diff_x, diff_y)
-    w = k_p*diff_x
-    if (abs(diff_x) > 130) and (abs(diff_x) < 150):
-        v -= k_p*abs(diff_x)
-        if v < 2:
-            v = 2
-    elif (abs(diff_x) > 150):
-        v -= k_p*abs(diff_x)
-        if v < 1:
-            v = 1
-    elif abs(diff_x) < 4:
-        v += 0.07
-        if v > 6:
-            v = 6
-    elif (abs(diff_x) > 60) and (abs(diff_x) < 130):
-        v = 1
-    else:
-        v += 0.01
-        if v > 5:
-            v = 5
-    return v, w
+    error = diff_x/480
+    i += 1
+    sum_error += error
+    print(error)
+    W = K_P*error + K_D*(error - prev_error) + K_I * sum_error/i
+    V = Vmax*pow(e, -1/150*abs(diff_x))
+    if V >= Vmax:
+        V = Vmax
+    return V , W, error
 
 HAL.setV(0)
 HAL.setW(0)
 while True:
-    try:
-        camera = HAL.getImage()
-        center = filter_image(camera)
-        v, w = get_speed(v, w, center)
-        HAL.setV(v)
-        HAL.setW(w)
-    except Exception as e:
-        print("CRASHED")
+    camera = HAL.getImage()
+    center = filter_image(camera)
+    V, W, prev_error = get_speed(V, W, center, prev_error, sum_error, i)
+    HAL.setV(V)
+    HAL.setW(W)
+    print(round(V*20, 0), "km/h")
+    print(round(W, 2), "rad/s")
 
